bed4411d3f166ccde0a606afe1ec6cbd1ceb4849
IGour <ijgour@gmail.com>
2019-03-30 22:32:27 +0530

    Initial check-in

---
+++ src/main/java/gour/indrajeet/tddsampleservice/TddSampleServiceApplication.java
@@ -0,0 +1,13 @@
+package gour.indrajeet.tddsampleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddSampleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddSampleServiceApplication.class, args);
+	}
+
+}
